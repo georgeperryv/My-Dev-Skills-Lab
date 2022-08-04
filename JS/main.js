178119372c1bfd7aad8d197bbefc83be8bf05d34
George Perry <george.e.perry5@gmail.com>
2022-08-03 19:16:11 -0700

    Third commit

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,6 @@
 // $(document).ready(function() {
+    
+
     $(".container").css({"display": "grid", "justify-content": "center", 
     "position": "absolute",
     "top": "50%", "left": "50%", "-ms-transform": "translate(-50%, -50%)",
@@ -13,19 +15,46 @@
 
     $("#add-skill").css({"width": "25%"});
 
-    $('ul').css({"list-style-type": "none", "display": "flex"});
+    $('ul').css({"list-style-type": "none", "display": "grid", "grid-template-columns": "1fr 20fr"});
 
-    $('#add-skill').on('click', function(evt) {
-        const $newRemoveButton = $('ul').append('<button class="remove-button" type="button"> X </button>');
-        $newRemoveButton.css({"align-content": "flex-start", "padding-left": "0px", "gap" : "30px"});
-        $('ul').append($('#skill-input').val());
+    $('#add-skill').on('click', function() {
+        const $newButton = '<li><button id="x-button" type="button"> X </button></li>'
+        $("ul").append($newButton);
+
+        const newSkill= $('input:text').val();
+        console.log(newSkill);
+        const $template = '<li>' + newSkill + '</li>';
+        $("ul").append($template); });
+        
+        // append('<button id="x-button" type="button"> X </button>');
+        // $newRemoveButton.css({"align-content": "flex-start", "padding-left": "0px", "gap" : "15px"});
+        // $('ul').append($('#skill-input').val());
+
+        
+
+        // $(this).fadeOut(1000,function() {
+        //     $(this).remove();
+        //     });
+        // });
      
-    });
 
-$('.remove-button').on('click', function(evt) {
-    console.log("hi"); });
+
+    $('ul').click('button', function() {
+            // let closest = $(this).closest('li');
+            // closest.remove();
+            // $(this).remove();
+
+            $(this).closest('li').remove(), function() {
+                $(this).remove();
+            };});
+
+            console.log("hi")
+
+        // console.log($('.remove-button'));
+    
+
    
-console.log($(".remove-button"));
+// console.log($(".remove-button"));
     //     $('ul').append("<li>hello<li>");
     //   });
       
